11836f4d1e80439aa452612bfe2bf0560253003c
Caseyann <goore_44@hotmail.ca>
2021-12-03 00:42:55 -0500

    updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,30 +1,22 @@
-const request = require('request');
-const args = process.argv.slice(2)
-
-const fs = require('fs');
+const request = require("request");
 
 
+const fetchBreedDescription = function(breedName, callback) {
+  let url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+    if (!error) {
+      const data = JSON.parse(body);
+      if (body.length <= 2 || breedName !== data[0].name) {
+        callback(breedName, null);
+      } else {
+        callback(null, data[0].description);
+      }
+    } else {
+      callback(error);
+    }
+  });
+};
 
 
-request(`https://api.thecatapi.com/v1/breedgs/search?q=${args}`, (error, response, body) => {
 
-if (error) {
-  return error;
-  console.log('Invalid Request')
-} else {
-  console.log(response && response.statusCode);
-}
-  const data = JSON.parse(body);
-// console.log(data);
-// console.log(typeof data);
-//console.log(data[0].description)
-
-
-  if ( data.length === 0 ) {
-    console.log("The Breed doesn't exist");
-  } else {
-    console.log(data[0].description);
-  }
-
-
-});
+module.exports = { fetchBreedDescription };
